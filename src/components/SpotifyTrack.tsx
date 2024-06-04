7b6cc98449125810a3f7c19882ef00a003317825
Morgscode <morgan.luke94@gmail.com>
2024-06-04 10:19:54 +0100

    chore: debug eslint rule

--- src/components/SpotifyTrack.tsx
+++ src/components/SpotifyTrack.tsx
@@ -4,5 +4,6 @@ import { type SpotifyTrack } from "@src/lib/spotify";
 
 export default function SpotifyTrack(props: SpotifyTrack) {
   console.log(JSON.stringify(props, null, 2));
+
   return <Text>Track</Text>;
 }
