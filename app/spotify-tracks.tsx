4617cbcd036d0694c37f2f2d6961c9dbda31dcc3
Morgscode <morgan.luke94@gmail.com>
2024-06-04 10:37:33 +0100

    chore: tidy up

--- app/spotify-tracks.tsx
+++ app/spotify-tracks.tsx
@@ -37,7 +37,8 @@ export default function SpotifyTracks() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-900">
       <Text className="text-gray-400 p-8">
-        These are your current top tracks
+        These are your current top tracks. Choose one of them to sample similar
+        music.
       </Text>
       <View className="flex flex-col">
         {tracks.map((track, index) => (
